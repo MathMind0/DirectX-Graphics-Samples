4bef90aff84f92113a63489d8232fa93ed98ef57
MathMind0 <tangjiangjun@hotmail.com>
2024-09-16 01:35:45 +0800

    [Postprocess Blur] Implementing naive blur.

--- Samples/Desktop/D3D12PostprocessBlur/src/shaders.hlsl
+++ Samples/Desktop/D3D12PostprocessBlur/src/shaders.hlsl
@@ -188,4 +188,33 @@ float4 VSPostprocess(uint vertexID : SV_VertexID)
     }  
 }
 
-float4 PSPostprocess()
+#define BLUR_RADIUS 5
+static const float weight[BLUR_RADIUS] = {0.3829, 0.2417, 0.0606, 0.0060, 0.0002};
+
+struct ScreenInfo
+{
+    uint2 size;
+};
+
+Texture2D texSceneColor : register(t0, PostprocessBlurNaive);
+ConstantBuffer<ScreenInfo> screenInfo : register(b0, PostprocessBlurNaive);
+
+float4 PSPostprocessBlurNaive(float4 screenPos : SV_Position) : SV_Target
+{
+    int2 screenPosI = floor(screenPos.xy);
+    
+    float4 color = 0.0;
+    [unroll]
+    for (int i = -BLUR_RADIUS + 1; i < BLUR_RADIUS; i++)
+    {
+        int row = clamp(screenPosI.y + i, 0, screenInfo.size.y - 1);
+        
+        for (int j = -BLUR_RADIUS + 1; j < BLUR_RADIUS; j++)
+        {
+            int col = clamp(screenPosI.x + j, 0, screenInfo.size.x - 1);
+            color += texSceneColor.Load(int2(col, row)) * weight[i] * weight[j];
+        }
+    }
+
+    return color;
+}
